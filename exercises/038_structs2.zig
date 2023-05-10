ee18dcee1f1800804bf0c259b0d6e1fd3d53f3d2
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-05-10 17:56:59 +0300

    fixed structs2.zig

--- exercises/038_structs2.zig
+++ exercises/038_structs2.zig
@@ -39,11 +39,12 @@ pub fn main() void {
     //     gold       10
     //     health     100
     //     experience 20
-    //
+
+    chars[1] = Character{ .class = Class.bard, .gold = 10, .health = 100, .experience = 20 };
+
     // Feel free to run this program without adding Zump. What does
     // it do and why?
 
-    // Printing all RPG characters in a loop:
     for (chars, 0..) |c, num| {
         std.debug.print("Character {} - G:{} H:{} XP:{}\n", .{
             num + 1, c.gold, c.health, c.experience,
